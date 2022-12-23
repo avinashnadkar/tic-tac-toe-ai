e7449182339196f4b9c53752193da2a4639310af
avinashnadkar <avinashnadkar3@gmail.com>
2022-12-23 20:39:05 +0530

    Created index page and basic game layout

--- app.js
+++ app.js
@@ -4,8 +4,17 @@ require('dotenv').config();
 const port = process.env.PORT || 3001
 const path = require('path');
 
+//express middlewares
+// serve your css as static
+app.use(express.static(__dirname + '/view'));
 
 
+//load index page
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname+'/view/index.html'));
+})
+
+//run server on port
 app.listen(port,()=>{
     console.log(`App is running on ${port}`)
 })
